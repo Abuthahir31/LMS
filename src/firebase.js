d26a47c7c0d3f18987b9ea20344b0555bc1de81d
Abuthahir31 <syedabuthahirabbas53@gmail.com>
2025-09-10 10:31:59 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcQLbLCItjBCONx8BmXyjOiDuIxbhRUVM",
-  authDomain: "uelms-378db.firebaseapp.com",
-  projectId: "uelms-378db",
-  storageBucket: "uelms-378db.firebasestorage.app",
-  messagingSenderId: "1004799765492",
-  appId: "1:1004799765492:web:111090f0932afdffcaa7a1",
-  measurementId: "G-G6N8HNRCVD"
+// your firebasekey
 };
 
 const app = initializeApp(firebaseConfig);
